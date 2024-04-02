bb8d6497d81ef1d53d5bc4ddcd413e0eadc91838
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-04-03 02:05:13 +0800

    Add new assets and update gitignore

---
+++ assets/CuteRPG_Field_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CuteRPG_Field_Tiles" tilewidth="16" tileheight="16" tilecount="384" columns="24">
+ <image source="Cute RPG World/Tilesets/CuteRPG_Field_Tiles.png" width="384" height="256"/>
+</tileset>
