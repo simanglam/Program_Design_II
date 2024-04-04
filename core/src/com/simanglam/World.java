645ecdf1598c9bb2fa26c0e68cee13c8d902ad10
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-04-04 15:28:17 +0800

    Remove unused import

--- core/src/com/simanglam/World.java
+++ core/src/com/simanglam/World.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Intersector;
