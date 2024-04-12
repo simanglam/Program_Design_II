6f4cfb7e63a9748fe49f532dec070eec2487c136
xup6m4c06 <xup6m4c060701@gmail.com>
2024-04-12 20:04:57 +0800

    Update map for midterm demo

---
+++ assets/CuteRPG_Village_House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CuteRPG_Village_House" tilewidth="16" tileheight="16" tilecount="1008" columns="48">
+ <image source="Cute RPG World/Tilesets/CuteRPG_Village_House.png" width="768" height="336"/>
+</tileset>
