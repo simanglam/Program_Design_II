690cb960caa60d318bce74095eba1327898fd14e
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-04-23 12:40:00 +0800

    Add vsync option to Lwjgl3ApplicationConfiguration in DesktopLauncher.java

--- desktop/src/com/simanglam/DesktopLauncher.java
+++ desktop/src/com/simanglam/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Main");
+		config.useVsync(true);
 		new Lwjgl3Application(new Main(), config);
 	}
 }
