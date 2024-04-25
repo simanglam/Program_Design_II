4083ae61a62588be497179f6bb8db46f1619b7d7
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-04-25 20:21:08 +0800

    Refactor BossWarActor class and add AttackInfo class in bosswar package

--- core/src/com/simanglam/fighting/bosswar/BossWarActor.java
+++ core/src/com/simanglam/fighting/bosswar/BossWarActor.java
@@ -1,50 +1,59 @@
 package com.simanglam.fighting.bosswar;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.TextureArray;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.simanglam.util.Const;
 
 public class BossWarActor{
     TextureRegion textures[][];
-    int healtPoint, current, forward, movingSpeed, direction;
-    float accu;
+    int healtPoint, current, forward, movingSpeed, direction, attackCoolDown, ATK;
+    float accu, attackAccu;
+    boolean attackable;
     Rectangle position;
+    Rectangle attackableRange;
+    Rectangle VisionRange;
 
     public BossWarActor(String path, boolean enemy){
-        init();
+        init(enemy);
         textures = new TextureRegion[3][2];
         int xoffset = 5, yoffset = 12;
         Texture t = new Texture(Gdx.files.internal(path));
         for (int y = 0; y < 2; y++){
             for (int x = 0; x < 3; x++){
-                textures[x][y] = new TextureRegion(t, 5 + ((14 + 10) * x), yoffset + ((14 + 12) * y), 14, 14);
+                textures[x][y] = new TextureRegion(t, 5 + ((14 + 10) * x), yoffset + ((14 + 10) * y), 14, 14);
             }
-            yoffset *= (enemy) ? 2 : 1;
+            yoffset += (enemy) ? 24 : 0;
         }
+        attackable = false;
     }
 
     public BossWarActor(TextureRegion[][] textureRegions, boolean enemy){
-        init();
+        init(enemy);
         this.textures = textureRegions;
 
     }
 
-    private void init(){
-        healtPoint = current = forward = 0;
-        direction = -1;
+    private void init(boolean enemy){
+        healtPoint = 10;
+        current = forward = 0;
+        attackAccu = 0;
+        attackCoolDown = 2;
+        direction = (enemy) ? 1 : -1;
         movingSpeed = 2;
-        position = new Rectangle(0, 60, 14, 14);
+        ATK = 10;
+        position = new Rectangle(Const.maxViewportWidth, 60, 14, 14);
+        attackableRange = new Rectangle(position);
+        attackableRange.width = 100;
     }
 
     public void update(float delta, boolean enemyInSight){
         forward = 0;
         accu += delta;
+        attackAccu += delta;
         if (accu >= 0.25){
             accu -= 0.25;
             current = (current + 1) % 3;
@@ -52,15 +61,36 @@ public class BossWarActor{
         if (!enemyInSight){
             position.x += movingSpeed * 20 * delta * direction;
             forward = 1;
+            attackable = false;
         }
-        //System.out.println(String.format("%f %f %d %d", position.x));
+        else{
+            attackable = attackCoolDown <= attackAccu;
+        }
     }
 
     public void draw(SpriteBatch batch){
         batch.draw(textures[current][forward], position.x, position.y);
     }
 
+    public boolean readyToAttack(){
+        return attackable;
+    }
+
     public AttackInfo generateAttackInfo(){
-        return new AttackInfo();
+        attackAccu = 0;
+        attackableRange.x = (direction > 0) ? position.x : position.x - attackableRange.width;
+        attackableRange.y = position.y;
+        return new AttackInfo((direction < 0) ? "player" : "enemy", attackableRange, ATK);
+    }
+
+    public void beingAttack(AttackInfo attackInfo){
+        if (Intersector.overlaps(position, attackInfo.demageRectangle)){
+            this.healtPoint -= attackInfo.damege;
+            System.out.println("Hello");
+        }
+    }
+
+    public void setPosition(float x, float y){
+        position.setPosition(x, y);
     }
 }
--- core/src/com/simanglam/fighting/bosswar/BossWarScreen.java
+++ core/src/com/simanglam/fighting/bosswar/BossWarScreen.java
@@ -1,7 +1,10 @@
 package com.simanglam.fighting.bosswar;
 
 import com.simanglam.Main;
+import com.simanglam.util.AbstractScreen;
 import com.simanglam.util.Const;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,7 +12,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class BossWarScreen implements Screen {
+public class BossWarScreen extends AbstractScreen {
     Main game;
     BossWarWorld world;
     Stage Stage;
@@ -22,6 +25,10 @@ public class BossWarScreen implements Screen {
         this.camera = new OrthographicCamera();
     }
 
+    public void handleInput(){
+        Gdx.input.setInputProcessor(world);
+    }
+
     public void render(float delta){
         ScreenUtils.clear(0, 0, 0, 0);
         this.world.update(delta, game.getSpriteBatch());
@@ -29,6 +36,7 @@ public class BossWarScreen implements Screen {
 
     public void resize(int x, int y){
         this.Stage.getViewport().update(x, y);
+        world.resize(x, y);
     }
 
     public void pause(){};
