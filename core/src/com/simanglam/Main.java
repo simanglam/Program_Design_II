50e9e67bc259d9c5f1df17465b9be278a7a83d96
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-04-25 20:19:28 +0800

    Refactor Main class to use AbstractScreen for screen management

--- core/src/com/simanglam/Main.java
+++ core/src/com/simanglam/Main.java
@@ -5,23 +5,33 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.simanglam.fighting.bosswar.BossWarScreen;
 import com.simanglam.map.MapScreen;
+import com.simanglam.util.AbstractScreen;
 import com.simanglam.util.InfoScreen;
 
 public class Main extends Game {
 	private SpriteBatch batch;
 	private Screen gameScreen;
 	private Screen infoScreen;
+	private Screen bossWarScreen;
 
 	public SpriteBatch getSpriteBatch(){return this.batch;}
 	public Screen getGameScreen(){return gameScreen;}
 	public Screen getInfoScreen(){return infoScreen;}
+	public Screen getBossWarScreen(){return bossWarScreen;}
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		infoScreen = new InfoScreen(this);
 		gameScreen = new MapScreen(this);
-		this.setScreen(new BossWarScreen(this));
+		bossWarScreen = new BossWarScreen(this);
+		this.setScreen(getBossWarScreen());
+	}
+
+	@Override
+	public void setScreen(Screen screen){
+		super.setScreen(screen);
+		((AbstractScreen)screen).handleInput();
 	}
 
 	@Override
