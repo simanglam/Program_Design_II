c406ab3d5af0a219d81a95811aa364172ed94f7e
jhenyulin <99192738+jhenyulin@users.noreply.github.com>
2024-05-13 16:27:45 +0800

    新增pokemon跟pokemonscreen

--- core/src/com/simanglam/Main.java
+++ core/src/com/simanglam/Main.java
@@ -1,48 +1,58 @@
-package com.simanglam;
-
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.simanglam.fighting.bosswar.BossWarScreen;
-import com.simanglam.map.MapScreen;
-import com.simanglam.util.AbstractScreen;
-import com.simanglam.util.InfoScreen;
 
 public class Main extends Game {
-	private SpriteBatch batch;
-	private Screen gameScreen;
-	private Screen infoScreen;
-	private Screen bossWarScreen;
+    private SpriteBatch batch;
+    private Screen gameScreen;
+    private Screen infoScreen;
+    private Screen bossWarScreen;
+    private Screen pokemonBattleScreen;
 
-	public SpriteBatch getSpriteBatch(){return this.batch;}
-	public Screen getGameScreen(){return gameScreen;}
-	public Screen getInfoScreen(){return infoScreen;}
-	public Screen getBossWarScreen(){return bossWarScreen;}
+    public SpriteBatch getSpriteBatch() {
+        return this.batch;
+    }
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		infoScreen = new InfoScreen(this);
-		gameScreen = new MapScreen(this);
-		bossWarScreen = new BossWarScreen(this);
-		this.setScreen(getBossWarScreen());
-	}
+    public Screen getGameScreen() {
+        return gameScreen;
+    }
 
-	@Override
-	public void setScreen(Screen screen){
-		super.setScreen(screen);
-		((AbstractScreen)screen).handleInput();
-	}
+    public Screen getInfoScreen() {
+        return infoScreen;
+    }
 
-	@Override
-	public void render () {
-		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		screen.dispose();
-		batch.dispose();
-	}
+    public Screen getBossWarScreen() {
+        return bossWarScreen;
+    }
+
+    public Screen getPokemonBattleScreen() {
+        return pokemonBattleScreen;
+    }
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        infoScreen = new InfoScreen(this);
+        gameScreen = new MapScreen(this);
+        bossWarScreen = new BossWarScreen(this);
+        pokemonBattleScreen = new PokemonBattleScreen(this);
+        this.setScreen(pokemonBattleScreen);
+    }
+
+    @Override
+    public void setScreen(Screen screen) {
+        super.setScreen(screen);
+        // 可以在這裡添加任何必要的處理
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        screen.dispose();
+        batch.dispose();
+    }
 }
-
--- core/src/com/simanglam/util/GameStatus.java
+++ core/src/com/simanglam/util/GameStatus.java
@@ -5,22 +5,34 @@ import java.util.ArrayList;
 import com.simanglam.fighting.Pokemon;
 
 public class GameStatus {
-    static GameStatus gameStatus;
+    private static GameStatus gameStatus;
     private ArrayList<InventoryItem> playerInventory;
-    private ArrayList<Pokemon> pokemons;
 
-
-    private GameStatus(){
+    private GameStatus() {
         playerInventory = new ArrayList<InventoryItem>();
     }
 
-    public static GameStatus getGameStatus(){
-        if (gameStatus == null)
+    public static GameStatus getInstance() {
+        if (gameStatus == null) {
             gameStatus = new GameStatus();
+        }
         return gameStatus;
     }
 
-    public static void clearGameStatus(){
-        GameStatus.gameStatus = null;
+    public void clearGameStatus() {
+        playerInventory.clear();
+        gameStatus = null;
+    }
+
+    // Add more methods as needed
+
+    // Example method to add an item to the player's inventory
+    public void addItemToInventory(InventoryItem item) {
+        playerInventory.add(item);
+    }
+
+    // Example method to get the player's inventory
+    public ArrayList<InventoryItem> getPlayerInventory() {
+        return playerInventory;
     }
 }
