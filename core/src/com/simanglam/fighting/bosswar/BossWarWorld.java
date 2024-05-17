cee9215deb065f8ba1fa03c4d4cc2ead70c4c476
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-05-17 21:56:05 +0800

    Let bosswar infomation can be stored as json file.

--- core/src/com/simanglam/fighting/bosswar/BossWarWorld.java
+++ core/src/com/simanglam/fighting/bosswar/BossWarWorld.java
@@ -2,7 +2,7 @@ package com.simanglam.fighting.bosswar;
 
 import java.util.ArrayList;
 
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.simanglam.util.Const;
@@ -17,6 +18,7 @@ import com.simanglam.util.Const;
 public class BossWarWorld extends InputAdapter {
     ArrayList<BossWarActor> pokemonArray;
     ArrayList<BossWarActor> enemyArray;
+    ArrayList<EnemySpawnInfo> enemySpawnInfos;
     ArrayList<AttackInfo> pendingAttack;
     BossWarActor enemyTower;
     BossWarActor playerTower;
@@ -24,29 +26,40 @@ public class BossWarWorld extends InputAdapter {
     Viewport viewport;
     OrthographicCamera camera;
     Vector3 lastTouchDown;
-    BossWarActor enemyProto;
-    BossWarActor playerProto;
     int money;
+    int maxEnemy;
+    float spawnAccu;
 
     public BossWarWorld(){
         pokemonArray = new ArrayList<>();
         enemyArray = new ArrayList<>();
         pendingAttack = new ArrayList<>();
+        enemySpawnInfos = new ArrayList<>();
         this.camera = new OrthographicCamera();
         camera.setToOrtho(false);
         this.viewport = new FitViewport(Const.maxViewportWidth / 2f, Const.maxViewportHeight / 2f, camera);
         this.viewport.getCamera().position.set(Const.maxViewportWidth / 4f, Const.maxViewportHeight / 4f, 0);
         this.viewport.apply();
-        enemyProto = new BossWarActor("enemies/base/", true);
-        playerProto = new BossWarActor("enemies/base/", false);
-        this.enemyTower = new BossWarActor(enemyProto);
+        this.enemyTower = new BossWarActor("enemies/base/", true);
         enemyTower.setPosition(0, 120);
-        this.playerTower = new BossWarActor(playerProto);
-        pokemonArray.add(new BossWarActor(playerProto));
-        enemyArray.add(new BossWarActor(enemyProto));
-        map = new Texture("bosswar.png");
+        this.playerTower = new BossWarActor("enemies/base/", false);
         lastTouchDown = new Vector3(0, 0, 0);
         this.money = 0;
+        Json json = new Json();
+        json.setElementType(BossWarInfo.class, "onstage", SpawnInfo.class);
+        json.setElementType(BossWarInfo.class, "enemies", SpawnInfo.class);
+        BossWarInfo bs = json.fromJson(BossWarInfo.class, Gdx.files.internal("bosswar/ex1.json"));
+        for (SpawnInfo info: bs.onstage){
+            for (int i = 0; i < info.spawnCoolDown; i++)
+                enemyArray.add(new BossWarActor("enemies/" + info.name + "/", true));
+        }
+        maxEnemy = 0;
+        spawnAccu = 1.0f;
+        for (SpawnInfo info: bs.enemies){
+            enemySpawnInfos.add(new EnemySpawnInfo(new BossWarActor("enemies/" + info.name + "/", true), info.spawnCoolDown));
+            maxEnemy = (maxEnemy < info.spawnCoolDown) ? info.spawnCoolDown : maxEnemy;
+        }
+        map = new Texture(bs.image);
     }
 
     public void centerCamera(){
@@ -60,35 +73,32 @@ public class BossWarWorld extends InputAdapter {
         return this.money;
     }
 
-    public boolean keyDown(int keycode){
-        if (keycode == Input.Keys.A)
-            pokemonArray.add(new BossWarActor(playerProto));
-        if (keycode == Input.Keys.D)
-            enemyArray.add(new BossWarActor(enemyProto));
-        return true;
-    }
-
+    @Override
     public boolean scrolled(float x, float y){
         if (viewport.getWorldWidth() + 10 * y <= map.getWidth() && viewport.getWorldHeight() + 10 * y <= map.getHeight() && viewport.getWorldWidth() + 10 * y > Const.maxViewportWidth / 2f&& viewport.getWorldHeight() + 10 * y > Const.maxViewportHeight / 2f){
             viewport.setWorldSize(viewport.getWorldWidth() + 10 * y, viewport.getWorldHeight() + 10 * y);
-            viewport.apply();
             if (this.camera.position.y - viewport.getWorldHeight() / 2 != 0)
                 this.camera.position.y -= this.camera.position.y - viewport.getWorldHeight() / 2;
         }
+        viewport.apply();
+        viewport.getCamera().update();
         centerCamera();
         return true;
     }
 
+    @Override
     public boolean touchDown(int x, int y, int pointer, int button){
         lastTouchDown.set(x, 0, 0);
         return true;
     }
 
+    @Override
     public boolean touchUp(int x, int y, int pointer, int button){
         lastTouchDown.set(0, 0, 0);
         return false;
     }
 
+    @Override
     public boolean touchDragged(int x, int y, int pointer){
         camera.position.add(lastTouchDown.sub(x, 0, 0));
         centerCamera();
@@ -98,16 +108,25 @@ public class BossWarWorld extends InputAdapter {
 
     public void resize(int x, int y){
         this.viewport.setScreenSize(x, y);
-        this.viewport.apply();
-        centerCamera();
         if (this.camera.position.y - viewport.getWorldHeight() / 2 != 0)
             this.camera.position.y -= this.camera.position.y - viewport.getWorldHeight() / 2;
+        this.viewport.apply();
         lastTouchDown.x = 0;
     }
 
     public void update(float delta, SpriteBatch batch){
         this.money += Math.max((int)delta, 1);
-        System.out.println(this.money);
+        spawnAccu += delta;
+        for (EnemySpawnInfo info: enemySpawnInfos){
+            if ((int)spawnAccu != 0 && (int)spawnAccu % info.spawnCoolDown == 0){
+                enemyArray.add(new BossWarActor(info.enemy));
+            }
+        }
+        if (spawnAccu >= maxEnemy){
+            spawnAccu -= maxEnemy;
+            System.out.println(spawnAccu);
+        }
+
         this.viewport.getCamera().update();
         batch.setProjectionMatrix(this.viewport.getCamera().combined);
         batch.begin();
@@ -127,27 +146,20 @@ public class BossWarWorld extends InputAdapter {
                 pendingAttack.add(actor.generateAttackInfo());
         }
         for (AttackInfo attackInfo: pendingAttack){
-            System.out.println(attackInfo);
             for (BossWarActor actor : (attackInfo.from.equals("enemy")) ? pokemonArray : enemyArray)
                 actor.beingAttack(attackInfo);
         }
         for (int i = 0; i < pokemonArray.size(); i++){
-            try {
                 BossWarActor actor = pokemonArray.get(i);
-                if (actor.healtPoint <= 0)
+                if (actor.healtPoint <= 0){
                     pokemonArray.remove(actor);
-            } catch (Exception e) {
-                System.err.println(e);
-            }
+                }
         }
         for (int i = 0; i < enemyArray.size(); i++){
-            try {
                 BossWarActor actor = enemyArray.get(i);
-                if (actor.healtPoint <= 0)
-                    enemyArray.remove(actor);
-            } catch (Exception e) {
-                System.err.println(e);
-            }
+                if (actor.healtPoint <= 0){
+                    boolean b = enemyArray.remove(actor);
+                }
         }
         pendingAttack.clear();
         batch.end();
@@ -163,4 +175,26 @@ public class BossWarWorld extends InputAdapter {
         }
         return false;
     }
+}
+
+class BossWarInfo {
+    public String image;
+    public ArrayList<SpawnInfo> onstage;
+    public ArrayList<SpawnInfo> enemies;
+}
+
+class SpawnInfo {
+    public String name;
+    int spawnCoolDown;
+}
+
+class EnemySpawnInfo {
+    BossWarActor enemy;
+    int spawnCoolDown;
+
+    public EnemySpawnInfo(BossWarActor proto, int coolDown) {
+        this.enemy = proto;
+        spawnCoolDown = coolDown;
+    }
+    
 }
