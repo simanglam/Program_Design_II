4275e7b50f1e737dd79cff7ce5b00f695f6dbd9a
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-05-19 01:51:46 +0800

    add select screen

---
+++ core/src/com/simanglam/fighting/bosswar/SelectScreen.java
@@ -0,0 +1,22 @@
+package com.simanglam.fighting.bosswar;
+
+import com.simanglam.util.AbstractScreen;
+
+public class SelectScreen extends AbstractScreen {
+
+    @Override
+    public void render(float delta) {
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        throw new UnsupportedOperationException("Unimplemented method 'resize'");
+    }
+
+    @Override
+    public void handleInput() {
+        throw new UnsupportedOperationException("Unimplemented method 'handleInput'");
+    }
+    
+}
