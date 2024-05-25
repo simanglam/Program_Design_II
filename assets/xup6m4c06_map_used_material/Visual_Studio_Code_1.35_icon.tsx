ca762dc1008672ccc083b812834f2e0917da7935
xup6m4c06 <xup6m4c060701@gmail.com>
2024-05-25 22:15:16 +0800

    new chamges

---
+++ assets/xup6m4c06_map_used_material/Visual_Studio_Code_1.35_icon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Visual_Studio_Code_1.35_icon" tilewidth="16" tileheight="16" tilecount="169" columns="13">
+ <image source="Visual_Studio_Code_1.35_icon.png" width="210" height="210"/>
+</tileset>
---
+++ assets/xup6m4c06_map_used_material/bosswar背景.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="bosswar背景" tilewidth="16" tileheight="16" tilecount="1058" columns="46">
+ <image source="../../../圖片/螢幕擷取畫面/螢幕擷取畫面 2024-05-25 105012.png" width="751" height="377"/>
+</tileset>
