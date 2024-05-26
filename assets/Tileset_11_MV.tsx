5c2fdcd61329fec97ed9519bb370fa3c77b4e710
xup6m4c06 <xup6m4c060701@gmail.com>
2024-05-27 00:22:00 +0800

    add stores in maps

---
+++ assets/Tileset_11_MV.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tileset_11_MV" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
+ <image source="Tileset_11_MV.png" width="768" height="768"/>
+</tileset>
---
+++ assets/stores.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="stores" tilewidth="16" tileheight="16" tilecount="462" columns="21">
+ <image source="stores.png" width="350" height="363"/>
+</tileset>
