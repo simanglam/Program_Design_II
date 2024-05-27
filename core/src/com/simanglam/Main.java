4b2ffe3de059780fbc03869902ad4305b4fbd13f
xup6m4c06 <xup6m4c060701@gmail.com>
2024-05-27 21:13:12 +0800

    changes in Main.java

--- core/src/com/simanglam/Main.java
+++ core/src/com/simanglam/Main.java
@@ -2,52 +2,41 @@ package com.simanglam;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.simanglam.fighting.MainPage;
-import com.simanglam.fighting.PokemonScreen;
-import com.simanglam.fighting.bosswar.BossWarScreen;
-import com.simanglam.fighting.bosswar.SelectScreen;
+import com.simanglam.fighting.SavePage;
 import com.simanglam.map.MapScreen;
 import com.simanglam.util.AbstractScreen;
 import com.simanglam.util.GameStatus;
 import com.simanglam.util.InfoScreen;
-import com.simanglam.fighting.PokemonScreen;
-import com.simanglam.fighting.MainPage;
-import com.simanglam.fighting.SavePage;
+import com.simanglam.util.LoadingScreen;
 
 public class Main extends Game {
-	private SpriteBatch batch;
-	private Screen gameScreen;
-	private Screen infoScreen;
-	private Screen bossWarScreen;
-	private Screen pokemonScreen;
+    private SpriteBatch batch;
+    private Screen gameScreen;
+    private Screen infoScreen;
+    private Screen bossWarScreen;
+    private Screen pokemonScreen;
     private Screen mainPage;
-	private Screen savePage;
+    private Screen savePage;
 
-	public AssetManager assetManager;
-
-	public SpriteBatch getSpriteBatch(){return this.batch;}
-	public Screen getGameScreen(){return gameScreen;}
-	public Screen getInfoScreen(){return infoScreen;}
-	public Screen getBossWarScreen(){return bossWarScreen;}
-	public Screen getPokemonScreen(){return pokemonScreen;}
+    public SpriteBatch getSpriteBatch(){return this.batch;}
+    public Screen getGameScreen(){return gameScreen;}
+    public Screen getInfoScreen(){return infoScreen;}
+    public Screen getBossWarScreen(){return bossWarScreen;}
+    public Screen getPokemonScreen(){return pokemonScreen;}
     public Screen getMainPage(){return mainPage;}
-	public Screen getSavePage(){return savePage;}
-	@Override
-	public void create () {
-		assetManager = new AssetManager();
-		GameStatus gameStatus = GameStatus.getGameStatus();
-		batch = new SpriteBatch();
-		infoScreen = new InfoScreen(this);
-		gameScreen = new MapScreen(this);
-		pokemonScreen = new PokemonScreen(this);
-    mainPage = new MainPage(this);
-		savePage = new SavePage(this);
-		this.setScreen(mainPage);
-		bossWarScreen = new BossWarScreen(this);
-		this.setScreen(new SelectScreen(this));
-	}
+    public Screen getSavePage(){return savePage;}
+    @Override
+    public void create () {
+        GameStatus gameStatus = GameStatus.getGameStatus();
+        batch = new SpriteBatch();
+        infoScreen = new InfoScreen(this);
+        gameScreen = new MapScreen(this);
+        mainPage = new MainPage(this);
+        savePage = new SavePage(this);
+        this.setScreen(new LoadingScreen(this));
+    }
 
     @Override
     public void setScreen(Screen screen) {
