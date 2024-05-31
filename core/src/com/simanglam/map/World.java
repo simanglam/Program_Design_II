92464a88ff1a5fec9e294724fd8b0c42a6a75823
xup6m4c06 <xup6m4c060701@gmail.com>
2024-05-31 13:36:13 +0800

    adding objects and generating-monster area in maps

--- core/src/com/simanglam/map/World.java
+++ core/src/com/simanglam/map/World.java
@@ -28,7 +28,7 @@ public class World {
     double ecounterPossibility;
 
     public World(){
-        this.tiledMap = new TmxMapLoader().load("library.tmx");
+        this.tiledMap = new TmxMapLoader().load("classroom.tmx");
         this.renderer = new OrthogonalTiledMapRenderer(tiledMap);
         this.camera = new OrthographicCamera();
         this.player = new Player();
