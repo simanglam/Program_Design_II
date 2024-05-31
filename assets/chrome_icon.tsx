f1d010287493740a5b0b4fb8284b6b105047aec8
xup6m4c06 <xup6m4c060701@gmail.com>
2024-05-31 14:43:12 +0800

    adding photo material source & editing maps

--- assets/chrome_icon.tsx
+++ assets/chrome_icon.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="chrome_icon" tilewidth="16" tileheight="16" tilecount="25" columns="5">
- <image source="../../../圖片/螢幕擷取畫面/螢幕擷取畫面 2024-05-25 205837.png" width="87" height="86"/>
+ <image source="螢幕擷取畫面 2024-05-25 205837.png" width="87" height="86"/>
 </tileset>
--- assets/icons.tsx
+++ assets/icons.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="icons" tilewidth="16" tileheight="16" tilecount="240" columns="30">
- <image source="../../../圖片/螢幕擷取畫面/螢幕擷取畫面 2024-05-25 212152.png" width="490" height="143"/>
+ <image source="螢幕擷取畫面 2024-05-25 212152.png" width="490" height="143"/>
 </tileset>
