06efbd233a48c60ae0cfa7df33596a9bca7c0290
xup6m4c06 <xup6m4c060701@gmail.com>
2024-06-01 16:19:20 +0800

    changes in computer_screen map

--- assets/final_project_A.tsx
+++ assets/final_project_A.tsx
@@ -1,5 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="final_project_A" tilewidth="22" tileheight="20" tilecount="20" columns="4">
- <grid orientation="orthogonal" width="20" height="18"/>
+<tileset version="1.10" tiledversion="1.10.2" name="final_project_A" tilewidth="18" tileheight="21" tilecount="25" columns="5">
  <image source="final_project_A.png" width="91" height="107"/>
 </tileset>
--- assets/final_project_C.tsx
+++ assets/final_project_C.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="final_project_C" tilewidth="16" tileheight="20" tilecount="30" columns="6">
- <image source="../../../../圖片/螢幕擷取畫面/螢幕擷取畫面 2024-05-25 214910.png" width="97" height="109"/>
+<tileset version="1.10" tiledversion="1.10.2" name="final_project_C" tilewidth="18" tileheight="21" tilecount="25" columns="5">
+ <image source="final_project_C.png" width="97" height="109"/>
 </tileset>
