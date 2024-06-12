025c69bba951cac3b99b7502c6687717320e093a
Si manglam <90334583+simanglam@users.noreply.github.com>
2024-06-12 21:24:14 +0800

    chore: Update enemy name in FightingScreen and description in SimpleAttack

--- core/src/com/simanglam/fighting/skillbehavior/SimpleAttack.java
+++ core/src/com/simanglam/fighting/skillbehavior/SimpleAttack.java
@@ -1,15 +1,14 @@
 package com.simanglam.fighting.skillbehavior;
 
-import java.util.ArrayList;
-
 import com.simanglam.fighting.Pokemon;
 
 public class SimpleAttack implements SkillBehavior {
     @Override
     public String description() {
-        return "HAHA";
+        return "測試用";
     }
-
+    
+    @Override
     public void update(Pokemon enemy, Pokemon self, Pokemon[] teams){
         enemy.beingAttack((int)(self.getATK() * 0.4f));
     } 
