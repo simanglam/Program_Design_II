c0ca80442b18a022575a02ad6e8ce1af0a643b18
xup6m4c06 <xup6m4c060701@gmail.com>
2024-06-13 00:44:46 +0800

    modifying computer_screen map

---
+++ assets/pc_icons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pc_icons" tilewidth="16" tileheight="16" tilecount="306" columns="18">
+ <image source="pc_icons.png" width="300" height="274"/>
+</tileset>
